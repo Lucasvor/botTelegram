b849de0ee5b3940136be3bc71f6e4379ee244fbc
Lucas <Lucas@github.com>
2018-12-08 12:32:39 -0200

    Add project files.

---
+++ botTelegramCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace botTelegramCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
